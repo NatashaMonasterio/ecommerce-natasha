7e51737a147d4b6b4531c959c6bf7d3d0dadb298
Natasha Monasterio <monasterio.tashi93@gmail.com>
2022-05-31 23:55:59 -0300

    Agregando Nav

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from './components/NavBar/NavBar';
+import Cuerpo from './components/Cuerpo/cuerpo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      < NavBar/>
+      < Cuerpo/>
+    </>
+  );
+}
+
+export default App;
